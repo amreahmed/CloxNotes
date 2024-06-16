dae4d0db1fce4717661149b1c5f66c19c0d4c3ca
Amr Ezzat <amr.2023001640@buc.edu.eg>
2024-06-16 05:30:32 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
